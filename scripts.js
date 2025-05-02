3a5f864986d24fb5686d2fffbb448fe1a8fbde78
Pentester Portfolio <livelikeaking202@gmail.com>
2025-05-02 13:20:05 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,4 +2,4 @@
 
 // This script can be used to add interactivity in the future.
 // For now, it's just a placeholder.
-console.log("Welcome to CAT IN THE HACK");
+console.log("Welcome to my Portfolio");
